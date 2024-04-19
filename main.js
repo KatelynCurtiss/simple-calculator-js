693091ba00550c3245527745b163707b2d46bc6a
Katelyn Curtiss <153852200+KatelynCurtiss@users.noreply.github.com>
2024-04-19 11:02:29 -0400

    updated the main JS

--- main.js
+++ main.js
@@ -4,11 +4,51 @@
 
 // Input 
 
+const firtsName = `Katelyn Curtiss`;
+const date = `19 APR 2024`;
+const name = `JS Simple Calculator`;
+
+const numOne = parseInt(prompt("Number 1:"));
+const numTwo = parseInt(prompt("Number 2:"));
+
+
 
 
 // Process
 
+// Addition
+const addResult = numOne + numTwo;
 
+// Subtraction
+const subResult = numOne - numTwo;
+
+
+// Multiplication 
+const multiResult = numOne * numTwo;
+
+
+//Dvision 
+const divResult = numOne / numTwo; 
 
 
 //Output 
+
+const myOutput = `
+
+Katelyn Curtiss
+19 APR 2024
+JS Simple Calculator
+
+
+
+
+
+Addition : ${addResult}
+
+Subtraction : ${subResult}
+
+Multiplication : ${multiResult}
+
+Division : ${divResult}`;
+
+console.log(myOutput); 
